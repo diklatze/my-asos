92cbc8d88545489f8ff2b48790e0f264a21da933
Dikla Tzemah Weyl <Dikla.Weyl@fundtech.isr>
2018-03-12 07:46:49 +0200

    semantic old

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,101 @@
 import { Component } from '@angular/core';
+import { ViewChild, Directive, ElementRef, OnDestroy, OnInit, Input } from '@angular/core';
+//import { NavController } from 'ionic-angular';
+declare var $: any
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-component',
+  templateUrl: 'app.component.html',
+  styles: [
+    `body {
+      background-color: #FFFFFF;
+    }`,
+
+    `.ui.menu .item img.logo {
+      margin-right: 1.5em;
+    }`,
+
+    `.main.container {
+      margin-top: 7em;
+    }`,
+
+    ` .wireframe {
+      margin-top: 2em;
+    }`,
+
+    `.ui.footer.segment {
+      margin: 5em 0em 0em;
+      padding: 0em 0em;`,
+  ]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  //  @ViewChild('descriptionAccordion') descriptionAccordionElementRef: ElementRef;
+  @ViewChild('menuPopup1') menuPopup1ElementRef: ElementRef;
+  // @ViewChild('menuPopup2') menuPopup2ElementRef: ElementRef;
+  @ViewChild('menuPopup3') menuPopup3ElementRef: ElementRef;  
+
+
+  constructor() { }
+
+
+  ngOnInit() {
+    //   $(this.descriptionAccordionElementRef.nativeElement)
+    //     .accordion();
+
+
+
+    $(this.menuPopup1ElementRef.nativeElement)
+      .popup({
+        inline: true,
+        hoverable: true,
+        position: 'bottom left',
+        delay: {
+          show: 300,
+          hide: 300
+        },
+        lastResort :'bottom left',
+        overflow:true,
+        setFluidWidth: false,
+        //onShow: () => { $('#popup1').css('max-width', $(window).width()); }
+      });
+
+
+    // $(this.menuPopup2ElementRef.nativeElement)
+    //   .popup({
+    //     inline: true,
+    //     hoverable: true,
+    //     position: 'bottom left',
+    //     delay: {
+    //       show: 300,
+    //       hide: 300
+    //     },
+    //      lastResort :'bottom left',
+    //      setFluidWidth: false,
+    //     overflow:true,
+    //   });
+
+
+      $(this.menuPopup3ElementRef.nativeElement)
+      .popup({
+        inline: true,
+        hoverable: true,
+        position: 'bottom left',
+        delay: {
+          show: 300,
+          hide: 300
+        },
+         lastResort :'bottom left',
+         setFluidWidth: false,
+        overflow:true,
+      });
+  }
+
+//  goToSignIn(){
+     
+//      this.router.navigate(['signIn']);
+
+//  }
+
 }
