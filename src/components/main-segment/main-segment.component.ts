1399abd5676a7bcf26962de8aa0258373323c261
Dikla Tzemah Weyl <Dikla.Weyl@fundtech.isr>
2018-03-21 14:53:01 +0200

    check if good

--- src/components/main-segment/main-segment.component.ts
+++ src/components/main-segment/main-segment.component.ts
@@ -35,7 +35,7 @@ export class MainSegmentComponent implements OnInit {
 
 
   callEveryAsos(index:number,store: String, lang: String, sizeSchema: String, currency: String) {
-    this.http.get('http://api.asos.com/product/catalogue/v2/products/' + this.productNumber + '?store=' + store + '&lang=' + lang + '=&sizeSchema=' + sizeSchema + '&currency=' + currency)
+    this.http.get('http://api.asos.com/product/catalogue/v2/products/' + this.productNumber + '?store=' + store + '&lang=' + lang + '&sizeSchema=' + sizeSchema + '&currency=' + currency)
       .map(res => {
         // If request fails, throw an Error that will be caught
         if (res.status < 200 || res.status >= 300) {
@@ -64,29 +64,29 @@ export class MainSegmentComponent implements OnInit {
     this.indexOfPrd = this.inputUrl.indexOf("prd/");
     this.productNumber = this.inputUrl.slice(this.indexOfPrd + 4, this.indexOfPrd + 11);
 
-    this.http.get('http://api.asos.com/product/catalogue/v2/products/' + this.productNumber + '?store=COM&lang=en-GB&sizeSchema=EU&currency=EUR')
-      .map(res => {
-        // If request fails, throw an Error that will be caught
-        if (res.status < 200 || res.status >= 300) {
-          throw new Error('This request has failed ' + res.status);
-        }
-        // If everything went fine, return the response
-        else {
-          return res.json();
-        }
-      })
-      .subscribe(
-      data => {
-        if (data) {
-          this.asosPrice = data;
-          this.asosPricesList[0] = this.asosPrice;
+    // this.http.get('http://api.asos.com/product/catalogue/v2/products/' + this.productNumber + '?store=COM&lang=en-GB&sizeSchema=EU&currency=EUR')
+    //   .map(res => {
+    //     // If request fails, throw an Error that will be caught
+    //     if (res.status < 200 || res.status >= 300) {
+    //       throw new Error('This request has failed ' + res.status);
+    //     }
+    //     // If everything went fine, return the response
+    //     else {
+    //       return res.json();
+    //     }
+    //   })
+    //   .subscribe(
+    //   data => {
+    //     if (data) {
+    //       this.asosPrice = data;
+    //       this.asosPricesList[0] = this.asosPrice;
 
-        }
+    //     }
 
-      },
-    );
+    //   },
+    // );
 
-    this.index = 0;
+    this.index = -1;
     this.searchParametersList.forEach(element => {
       this.index= this.index+1;
       this.callEveryAsos(this.index,element.store,element.lang,element.sizeSchema,element.currency);
